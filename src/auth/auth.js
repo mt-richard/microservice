95963cb0ab649d76a66fdbe89138e36b90ad423d
mt-richard <Richard@festimoltd.com>
2023-05-26 09:39:48 +0200

    Adding about page

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -6,7 +6,9 @@ function start() {
   app.get('/', (req, res) => {
     res.send('Auth API by Richard.');
   });
-
+  app.get('/about', (req, res) => {
+    res.json({status: '200', message: "Welcome to my API"});
+  });
   app.listen(port, () => {
     console.log(`Server -Auth is listening on port ${port}.`);
   });
