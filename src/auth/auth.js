9639c2f454de601d778f92fc9a610986facce666
mt-richard <Richard@fesimoltd.com>
2023-06-22 15:49:04 +0200

    Nodemailer

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
 const port = 3001; 
 
@@ -7,9 +8,33 @@ function start() {
     res.send('Auth API by Richard.');
   });
 
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth : {
+      user : "richardtuyishime43@gmail.com",
+      pass: "eoareecletswmxap"
+    }
+  });
+
+  const mailOptions = {
+    form: "richardtuyishime43@gmail.com",
+    to: "mbanzatrichard@gmail.com",
+    subject: "Testing NOdeMailer",
+    text: "I think its working as well!!!"
+  };
+  transport.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("email sent: " + info.response);
+    }
+  })
+
   app.listen(port, () => {
     console.log(`Server -Auth is listening on port ${port}.`);
   });
+
+
 }
 
 module.exports = { start };
